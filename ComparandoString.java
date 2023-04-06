3b79e36141d1bb39276cd48a5a1fc555dec33edb
Victor Hugo <69818265+victor7553@users.noreply.github.com>
2023-04-06 12:37:59 -0300

    Add files via upload

---
+++ ComparandoString.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class ComparandoString {
+	public static void main(String[] args) {
+		
+		System.out.println("4" == "4");
+		
+		String s1 = new String("2");
+		System.out.println("2" == s1);
+		System.out.println("2".equals(s1));
+		
+	}
+}
